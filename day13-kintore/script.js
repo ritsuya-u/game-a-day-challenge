78b57c71f6e77c8e2e22662d1e90ed1f8401f7ae
ritsuya-u <uramoto.ritsuya@gmail.com>
2026-02-13 22:46:39 +0900

    Add day13-kintore

---
+++ day13-kintore/script.js
@@ -0,0 +1,53 @@
+﻿const squatOrder = [1, 2, 3, 3, 3, 3, 2, 1];
+const frameMs = 80;
+
+const trainerButton = document.getElementById("trainer-button");
+const trainerImage = document.getElementById("trainer-image");
+const mushroom = document.getElementById("mushroom");
+const squatCount = document.getElementById("count");
+
+let animating = false;
+let mushroomScale = 1;
+let count = 0;
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function pickRandomMushroom() {
+  const id = Math.floor(Math.random() * 4) + 1;
+  mushroom.src = `images/kinoko_${id}.webp`;
+}
+
+function growMushroom() {
+  mushroomScale += 0.05;
+  mushroom.style.transform = `scale(${mushroomScale.toFixed(2)})`;
+}
+
+function squatCountPlas() {
+  count += 1;
+  squatCount.textContent = `${count}回`
+
+}
+
+async function playSquat() {
+  if (animating) {
+    return;
+  }
+
+  animating = true;
+  trainerButton.disabled = true;
+
+  for (const frame of squatOrder) {
+    trainerImage.src = `images/${frame}.webp`;
+    await wait(frameMs);
+  }
+
+  growMushroom();
+  squatCountPlas();
+  trainerButton.disabled = false;
+  animating = false;
+}
+
+pickRandomMushroom();
+trainerButton.addEventListener("click", playSquat);
